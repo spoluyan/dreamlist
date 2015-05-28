2bdd2eee9cefd0be19355bd225a97915da34b7ff
Sergey Poluyan <spoluyan@exadel.com>
2015-05-28 13:58:32 +0300

    Error 404 page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,7 +4,6 @@ import play.mvc.Controller;
 import play.mvc.With;
 
 @With(Secure.class)
-// TODO override error pages
 public class Application extends Controller {
 
     public static void index() {
