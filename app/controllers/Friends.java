cf86227dff86add0d84045f63218c563dea503cf
Sergey Poluyan <spoluyan@exadel.com>
2015-05-29 15:16:36 +0300

    Rename controller

--- app/controllers/Friends.java
+++ app/controllers/Friends.java
@@ -4,7 +4,7 @@ import models.User;
 import play.mvc.Controller;
 
 //TODO implement
-public class AddFriends extends Controller {
+public class Friends extends Controller {
     public static void addFriend(String login) {
         User current = User.findById(Security.getUserID());
         User friend = User.findByLogin(login);
